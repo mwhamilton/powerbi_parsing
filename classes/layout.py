2ce022ce181dee116d3f77bf4f6f3899f00b4572
MHamilton <mhamilton@transitchicago.com>
2021-05-15 15:12:23 -0500

    added __dict__ attributes to help json reserialization

--- classes/layout.py
+++ classes/layout.py
@@ -1,15 +1,25 @@
 class Layout:
     def __init__(self, raw_data):
+        print(raw_data['sections'])
         self.id = raw_data['id']
         self.resourcePackages = raw_data['resourcePackages']
         self.sections = [Section(raw_section_data) for raw_section_data in raw_data['sections']]
         self.config = raw_data['config']
         self.layoutOptimization = raw_data['layoutOptimization']
 
+    def __dict__(self):
+        return {
+            'id': self.id,
+            'resourcePackages': self.resourcePackages,
+            'sections': [x.__dict__() for x in self.sections],
+            'config': self.config,
+            'layoutOptimization': self.layoutOptimization
+        }
+
 
 class Section:
     def __init__(self, raw_data):
-        self.id = raw_data['id']
+        self.id = raw_data.get('id')
         self.name = raw_data['name']
         self.display_name = raw_data['displayName']
         self.filters = raw_data['filters']
@@ -18,3 +28,17 @@ class Section:
         self.config = raw_data['config']
         self.dispay_option = raw_data['displayOption']
         self.dimensions = {'width': raw_data['width'], 'height': raw_data['height']}
+
+    def __dict__(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'displayName': self.display_name,
+            'filters': self.filters,
+            'ordinal': self.ordinal,
+            'visualContainers': self.visualContainers,
+            'config': self.config,
+            'displayOption': self.dispay_option,
+            'width': self.dimensions['width'],
+            'height': self.dimensions['height'],
+        }
