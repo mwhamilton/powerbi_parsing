e67b38ce936aa4c7859625230fec63453ad1d8b9
MHamilton <mhamilton@transitchicago.com>
2021-05-15 14:40:10 -0500

    Doing basic deserialization on the powerbi

--- extract.py
+++ extract.py
@@ -1,5 +1,8 @@
 import os
 import zipfile
+import json
+from bs4 import BeautifulSoup
+from pprint import pprint
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -11,9 +14,39 @@ class PowerBI:
     def _get_zip(self):
         return zipfile.ZipFile(self.file_path, "r")
 
+    def get_content_types(self):
+        return BeautifulSoup(self.zip.open('[Content_Types].xml').read().decode('utf-8'), 'lxml')
+
+    def get_data_model_schema(self):
+        return json.loads(self.zip.open('DataModelSchema').read().decode('utf-16-le'))
+
+    def get_diagram_layout(self):
+        return json.loads(self.zip.open('DiagramLayout').read().decode('utf-16-le'))
+
+    def get_layout(self):
+        return json.loads(self.zip.open('Report/Layout').read().decode('utf-16-le'))
+
     def get_metadata(self):
-        return self.zip.open('Metadata', 'r').read().decode('utf-16-le')
+        return json.loads(self.zip.open('Metadata', 'r').read().decode('utf-16-le'))
+
+    def get_security_bindings(self):
+        raise NotImplementedError
+
+    def get_settings(self):
+        return json.loads(self.zip.open('Settings', 'r').read().decode('utf-16-le'))
+
+    def get_static_resources(self):
+        static_resources = []
+        for file_path in self.zip.namelist():
+            if file_path.startswith("Report/StaticResources"):
+                static_resources.append({
+                    'file_path': file_path,
+                    'data': json.loads(self.zip.open(file_path, 'r').read().decode('utf-8'))
+                })
+        return static_resources
+
+    def get_version(self):
+        return self.zip.open('Version', 'r').read().decode('utf-16-le')
 
 
 powerbi = PowerBI('test.pbit')
-powerbi.get_metadata()
